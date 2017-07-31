7e354a3f08de05ce9aeeb2fe2f901f6ee47ccfdf
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2017-07-31 17:10:43 +0200

    initial commit

--- README.md
+++ README.md
@@ -6,7 +6,12 @@ This project is a quick study for home automation with NodeMCU WiFi and an RGB L
   - NodeMCU
   - Transistors
   - Voltage regulators
+  - Resistances
   - RGB LED strip
 ### SW Components
   - IDE: Arduino 1.8
+  - ESP8266 Board family for Arduino
+  - WiFi AP (currently a Raspberry Pi3)
+  - WifiManager library for Arduino
+
 
---
+++ nodemcu-rgbstrip.ino
@@ -0,0 +1,26 @@
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+
+#define FSTR(x) (__FlashStringHelper*)(x)
+const char HTTP[]  PROGMEM  = "Content-type: text/html";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  char *a =(char*)FSTR(HTTP); 
+  Serial.println('Setup...');
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
