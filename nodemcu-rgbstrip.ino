a45c68de1655d6a4a37d05fa579ee81b65abf6e5
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2017-08-31 18:29:26 +0200

    strings to PROGMEM

--- nodemcu-rgbstrip.ino
+++ nodemcu-rgbstrip.ino
@@ -6,11 +6,10 @@
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
 
-/* Store strings in PROGMEM
-
-*/
+/* Store strings in PROGMEM */
 const char CANNOT_CONNECT[] PROGMEM = {'Cannot connect to the AP: '};
 const char MOUNTFAIL[] PROGMEM = {'Failed to mount file system!'};
+const char FSMOUNTED[] PROGMEM = {'mounted file system'};
 const char SAVEWCONF[] PROGMEM = {'Saving the WiFi configuration...'};
 const char CONFMODE[] PROGMEM = {'Entered to configuration mode...'};
 const char RESETNODE[] PROGMEM = {'Entered to configuration mode...'};
@@ -19,11 +18,27 @@ const char SETUPSTR[] PROGMEM = {'Setup...'};
 const char MACMSG[] PROGMEM = {'MAC Address: '};
 const char APIPSTR[] PROGMEM = {'AP IP: '};
 const char NOCONF[] PROGMEM = {'Config file not found!'};
+const char CONFOPEN[] PROGMEM = {'saveConfigCallback: opened config file for writing the config'};
+const char CONFFAILOPEN[] PROGMEM = {'saveConfigCallback: cannot open the config file for write'};
+const char CONFOPENSAVE[] PROGMEM = {'saveConfigCallback: config file has been opened for savig the config and JSON buffer has been created successfully'};
+const char CONFSAVED[] PROGMEM = {'saveConfigCallback: config save ok'};
+const char CONFOPENED[] PROGMEM = {'config opened for read'};
+const char CONFOPENING[] PROGMEM = {'opening the config file...'};
+const char CONFPARSED[] PROGMEM = {'\nparsed json'};
+const char CONFPARSEFAIL[] PROGMEM = {'failed to parse the config'};
+const char CONFDEL[] PROGMEM = {'Deleting the saved config file...'};
+const char RESET[] PROGMEM = {'Reset...'};
+const char WIFIOK[] PROGMEM = {'Connected'};
+const char WIFIFAIL[] PROGMEM = {'WiFi: failed to connect to the AP'};
+const char WIFICONF[] PROGMEM = {'Current config: '};
+const char WIFIIP[] PROGMEM = {'IP: '};
 
+/* Global constants */
+#define MAX_CONNTRY 5
 #define PWM_FREQ 100
-#define AP_PASSW "xxx0xxx"
 #define CONFNAME "/wificreds.conf"
 
+/* Global variables */
 ESP8266WebServer myServer(8080);
 String stassid = "";
 String stapass = "";
@@ -31,9 +46,9 @@ String stapass = "";
 void deleteWifiConfig() {
   cli();
   if (SPIFFS.exists(CONFNAME)) {
-    Serial.println("Deleting the saved config file...");
+    Serial.println(FPSTR(CONFDEL));
     SPIFFS.remove(CONFNAME);
-    Serial.println("Restart...");
+    Serial.println(FPSTR(RESET));
     ESP.reset();   
   }
 }
@@ -45,18 +60,18 @@ void saveWifiConfigCallback() {
   stapass = WiFi.psk();
   File configFile = SPIFFS.open(CONFNAME, "w");
   if (configFile) {
-        Serial.println("saveConfigCallback: opened config file for writing the config");
+        Serial.println(FPSTR(CONFOPEN));
   } else {
-        Serial.println("saveConfigCallback: cannot open the config file for write");
+        Serial.println(FPSTR(CONFFAILOPEN));
         return;
   }
-  Serial.println("saveConfigCallback: config file has been opened for savig the config and JSON buffer has been created successfully");
+  Serial.println(FPSTR(CONFOPENSAVE));
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& json = jsonBuffer.createObject();
   json["sta_ssid"] = stassid;
   json["sta_pass"] = stapass;
   json.printTo(configFile);
-  Serial.println("saveConfigCallback: config save ok");
+  Serial.println(FPSTR(CONFSAVED));
 }
 
 void configModeCallback(WiFiManager *myWfMan) {
@@ -83,18 +98,19 @@ void setup() {
   /* remove saved config and restart if the flash button is pushed */
   attachInterrupt(digitalPinToInterrupt(D3), deleteWifiConfig, CHANGE);
 
+/* Setup PWM */
   analogWriteFreq(PWM_FREQ);
   analogWriteRange(1023);
 
- // Try to load config
+ /* Try to load config */
  if (SPIFFS.begin()) {
-    Serial.println("mounted file system");
+    Serial.println(FPSTR(FSMOUNTED));
     if (SPIFFS.exists(CONFNAME)) {
       //file exists, reading and loading
-      Serial.println("reading config file");
+      Serial.println(FPSTR(CONFOPENING));
       File configFile = SPIFFS.open(CONFNAME, "r");
       if (configFile) {
-        Serial.println("opened config file");
+        Serial.println(FPSTR(CONFOPENED));
         size_t size = configFile.size();
         // Allocate a buffer to store contents of the file.
         std::unique_ptr<char[]> buf(new char[size]);
@@ -104,20 +120,20 @@ void setup() {
         JsonObject& json = jsonBuffer.parseObject(buf.get());
         json.printTo(Serial);
         if (json.success()) {
-          Serial.println("\nparsed json");
+          Serial.println(FPSTR(CONFPARSED));
           stassid = String((const char*)json["sta_ssid"]);
           stapass = String((const char*)json["sta_pass"]);
           hasSavedConfig = true;
         } else {
-          Serial.println("failed to load json config");
+          Serial.println(FPSTR(CONFPARSEFAIL));
         }
       }
     } else {
-      Serial.println("No config file");
+      Serial.println(FPSTR(NOCONF));
     }
   } else {
-    Serial.println("failed to mount FS");
-    Serial.println("Reset...");
+    Serial.println(FPSTR(MOUNTFAIL));
+    Serial.println(FPSTR(RESET));
     Serial.println("");
     ESP.reset();
   }
@@ -139,10 +155,11 @@ void setup() {
   /* Connect to the AP if not connected by the WiFiManager */
   WiFi.setAutoConnect(false);
   WiFi.setAutoReconnect(true);
-  for (uint8_t count = 5; count > 0; count--) {
+  for (uint8_t count = MAX_CONNTRY; count > 0; count--) {
     if (!WiFi.isConnected()) {
       WiFi.begin(stassid.c_str(), stapass.c_str());
     } else {
+      Serial.println(FPSTR(WIFIFAIL));
       break;
     }
     delay(1000);
@@ -158,15 +175,16 @@ void setup() {
   } else {
     WiFi.setAutoConnect(true);
     Serial.println("");
-    Serial.println("Connected");
-    Serial.println("Current config: ");
+    Serial.println(FPSTR(WIFIOK));
+    Serial.println(FPSTR(WIFICONF));
     WiFi.printDiag(Serial);
-    Serial.print("IP: ");
+    Serial.print(FPSTR(WIFIIP));
     Serial.println(WiFi.localIP());
   }
 
   /*
       TODO: Setup the webserver (myServer)
+      TODO: Setup the MQTT client
   */
 }
 
