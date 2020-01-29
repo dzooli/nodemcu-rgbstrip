ec9cd2675d645032a13f080fc31733f2016abac9
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2020-01-29 01:19:47 +0100

    MQTT payload parser started. MQTT connection ok with callback

--- nodemcu-rgbstrip.ino
+++ nodemcu-rgbstrip.ino
@@ -2,59 +2,103 @@
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+#include <PubSubClient.h>
+#include <strings.h>
 
-/* Store strings in PROGMEM */
-const char CANNOT_CONNECT[] PROGMEM = {'Cannot connect to the AP: '};
-const char MOUNTFAIL[] PROGMEM = {'Failed to mount file system!'};
-const char FSMOUNTED[] PROGMEM = {'mounted file system'};
-const char SAVEWCONF[] PROGMEM = {'Saving the WiFi configuration...'};
-const char CONFMODE[] PROGMEM = {'Entered to configuration mode...'};
-const char RESETNODE[] PROGMEM = {'Entered to configuration mode...'};
-const char CONFSTR[] PROGMEM = {'Config: '};
-const char SETUPSTR[] PROGMEM = {'Setup...'};
-const char MACMSG[] PROGMEM = {'MAC Address: '};
-const char APIPSTR[] PROGMEM = {'AP IP: '};
-const char NOCONF[] PROGMEM = {'Config file not found!'};
-const char CONFOPEN[] PROGMEM = {'saveConfigCallback: opened config file for writing the config'};
-const char CONFFAILOPEN[] PROGMEM = {'saveConfigCallback: cannot open the config file for write'};
-const char CONFOPENSAVE[] PROGMEM = {'saveConfigCallback: config file has been opened for savig the config and JSON buffer has been created successfully'};
-const char CONFSAVED[] PROGMEM = {'saveConfigCallback: config save ok'};
-const char CONFOPENED[] PROGMEM = {'config opened for read'};
-const char CONFOPENING[] PROGMEM = {'opening the config file...'};
-const char CONFPARSED[] PROGMEM = {'\nparsed json'};
-const char CONFPARSEFAIL[] PROGMEM = {'failed to parse the config'};
-const char CONFDEL[] PROGMEM = {'Deleting the saved config file...'};
-const char RESET[] PROGMEM = {'Reset...'};
-const char WIFIOK[] PROGMEM = {'Connected'};
-const char WIFIFAIL[] PROGMEM = {'WiFi: failed to connect to the AP'};
-const char WIFICONF[] PROGMEM = {'Current config: '};
-const char WIFIIP[] PROGMEM = {'IP: '};
-const char WIFICONNECTTRY[] PROGMEM = {'Trying to connect to the AP... '};
+#include "DomoticzRGBDimmer.h"
+
+/* New PWM example - should be removed from the final code
+ *
+#define PWM_CHANNELS 5
+const uint32_t period = 5000; // * 200ns ^= 1 kHz
+
+// PWM setup
+uint32 io_info[PWM_CHANNELS][3] = {
+  // MUX, FUNC, PIN
+  {PERIPHS_IO_MUX_MTDI_U,  FUNC_GPIO12, 12},
+  {PERIPHS_IO_MUX_MTDO_U,  FUNC_GPIO15, 15},
+  {PERIPHS_IO_MUX_MTCK_U,  FUNC_GPIO13, 13},
+  {PERIPHS_IO_MUX_MTMS_U,  FUNC_GPIO14, 14},
+  {PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5 ,  5},
+};
+
+// initial duty: all off
+uint32 pwm_duty_init[PWM_CHANNELS] = {0, 0, 0, 0, 0};
+
+pwm_init(period, pwm_duty_init, PWM_CHANNELS, io_info);
+pwm_start();
+
+// do something like this whenever you want to change duty
+pwm_set_duty(500, 1);  // GPIO15: 10%
+pwm_set_duty(5000, 1); // GPIO15: 100%
+pwm_start();           // commit
+ *
+*/
+
+/* PWM constants and variables */
+#include <eagle_soc.h>
+#define PWM_CHANNELS 3
+const uint32_t pwm_period = 5000; // * 200ns ^= 1 kHz
+uint32 io_info[PWM_CHANNELS][3] = {
+  // MUX, FUNC, PIN
+  {PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5, 20},
+  {PERIPHS_IO_MUX_GPIO4_U, FUNC_GPIO4, 19},
+  {PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2, 17},  
+};
+// initial duty: all off
+uint32 pwm_duty_init[PWM_CHANNELS] = {0, 0, 0};
+/* PWM LEDS -> PWM channels in io_info */
+#define PWM_R     0
+#define PWM_G     1
+#define PWM_B     2
+
+/*
+ * TODO: PWM value storage
+ *   - store the values into EEPROM before OFF state
+*/ 
+
+/* Program states and the current state */
+enum {
+  MODE_OFF,
+  MODE_ON,
+  MODE_FULL,
+  MODE_SLEEP,
+  MODE_FLASH,
+  MODE_SMOOTH,
+  MODE_LOVE
+};
+
+uint8_t currState = MODE_OFF;
 
 /* Global constants */
+#define MQTT_SERVER_PORT 1883
 #define MAX_CONNTRY 5
 #define PWM_FREQ 100
 #define CONFNAME "/wificreds.conf"
 
 #define SLEDS_OFF { digitalWrite(D6, false); digitalWrite(D7, false); digitalWrite(D8, false); }
-#define CAPTIVE_ON { digitalWrite(D6, false); digitalWrite(D7, false); digitalWrite(D8, true); }
-#define BOOT_ON { digitalWrite(D6, true); digitalWrite(D7, true); digitalWrite(D8, false); }
-#define ERROR_ON { digitalWrite(D6, true); digitalWrite(D7, false); digitalWrite(D8, false); }
-#define OK_ON { digitalWrite(D6, false); digitalWrite(D7, true); digitalWrite(D8, false); }
+#define CAPTIVE_ON { digitalWrite(D6, false); digitalWrite(D7, false); digitalWrite(D8, true); }  // Blue LED
+#define BOOT_ON { digitalWrite(D6, true); digitalWrite(D7, true); digitalWrite(D8, false); }      // Yellow LED
+#define ERROR_ON { digitalWrite(D6, true); digitalWrite(D7, false); digitalWrite(D8, false); }    // Red LED
+#define OK_ON { digitalWrite(D6, false); digitalWrite(D7, true); digitalWrite(D8, false); }       // Green LED
 
 /*
- * Definitions for the MQTT parameters
- */
+   Definitions for the MQTT parameters
+*/
 #define MQTT_CLEN 80 // channel string length
-#define MQTT_ULEN 10 // username length
-#define MQTT_PLEN 10 // password length
-#define MQTT_FLEN 12 // friendly name length  
+#define MQTT_ULEN 20 // username length
+#define MQTT_PLEN 20 // password length
+#define MQTT_FLEN 20 // friendly name length  
 
 #define DEBUG 1
 
 /* Global variables */
-//ESP8266WebServer myServer(8080);
+WiFiClient  wfClient;
+PubSubClient mqttC(wfClient); // The MQTT client
 WiFiManager wfMan;
+
+bool connectMQTT(const char* user, const char* pass, const char* topic, const char* clientid);
+void mqttCallback(char* topic, uint8_t* payload, unsigned int len);
 
 String stassid = "";
 String stapass = "";
@@ -63,34 +107,27 @@ String mqtt_fname = "";
 String mqtt_user = "";
 String mqtt_pass = "";
 
-WiFiManagerParameter mqttchannel("mqttchannel", "MQTT Channel",(const char*)mqtt_channel.c_str(), MQTT_CLEN);
-WiFiManagerParameter mqttfname("mqttfname", "MQTT Name",(const char*)mqtt_fname.c_str(), MQTT_FLEN);
+WiFiManagerParameter mqttchannel("mqttchannel", "MQTT Channel", (const char*)mqtt_channel.c_str(), MQTT_CLEN);
+WiFiManagerParameter mqttfname("mqttfname", "MQTT Name", (const char*)mqtt_fname.c_str(), MQTT_FLEN);
 WiFiManagerParameter mqttuser("mqttuser", "MQTT User", (const char*)mqtt_user.c_str(), MQTT_ULEN);
-WiFiManagerParameter mqttpass("mqttpass", "MQTT Password",(const char*)mqtt_pass.c_str(), MQTT_PLEN);
+WiFiManagerParameter mqttpass("mqttpass", "MQTT Password", (const char*)mqtt_pass.c_str(), MQTT_PLEN);
 
 void deleteWifiConfig() {
-  cli();
+  noInterrupts();
   if (SPIFFS.exists(CONFNAME)) {
-    Serial.println(FPSTR(CONFDEL));
     SPIFFS.remove(CONFNAME);
-    Serial.println(FPSTR(RESET));
-    ESP.reset();   
+    ESP.reset();
   }
 }
 
 void saveWifiConfigCallback() {
-  Serial.println(FPSTR(SAVEWCONF));
   // Store the credentials for using it in the program
   stassid = WiFi.SSID();
   stapass = WiFi.psk();
   File configFile = SPIFFS.open(CONFNAME, "w");
-  if (configFile) {
-        Serial.println(FPSTR(CONFOPEN));
-  } else {
-        Serial.println(FPSTR(CONFFAILOPEN));
-        return;
+  if (!configFile) {
+    return;
   }
-  Serial.println(FPSTR(CONFOPENSAVE));
   StaticJsonBuffer<400> jsonBuffer;
   JsonObject& json = jsonBuffer.createObject();
   json["sta_ssid"] = stassid;
@@ -100,14 +137,11 @@ void saveWifiConfigCallback() {
   json["mqtt_user"] = mqttuser.getValue();
   json["mqtt_pass"] = mqttpass.getValue();
   json.printTo(configFile);
-  Serial.println(FPSTR(CONFSAVED));
 }
 
 void configModeCallback(WiFiManager *myWfMan) {
-  Serial.println(FPSTR(CONFMODE));
-  Serial.print(FPSTR(APIPSTR));
+  Serial.println(F("Captive portal parameters:"));
   Serial.println(WiFi.softAPIP());
-  Serial.print(FPSTR(MACMSG));
   Serial.println(WiFi.softAPmacAddress());
   Serial.println(myWfMan->getConfigPortalSSID());
 }
@@ -117,7 +151,6 @@ void setup() {
 
   delay(500);
   Serial.begin(115200);
-  Serial.println(FPSTR(SETUPSTR));
 
   pinMode(D1, OUTPUT);  /* RED */
   pinMode(D2, OUTPUT);  /* GREEN */
@@ -130,38 +163,29 @@ void setup() {
   pinMode(D8, OUTPUT);  // BLUE status LED
 
   // STATUS LED TEST
-  SLEDS_OFF;
-  delay(500);
-  SLEDS_OFF;
-  CAPTIVE_ON;
-  delay(500);
-  SLEDS_OFF;
-  BOOT_ON;
-  delay(500);
-  SLEDS_OFF;
-  ERROR_ON;
-  delay(500);
-  SLEDS_OFF;
-  OK_ON;
-  delay(500);
-  SLEDS_OFF;
-  
+  SLEDS_OFF;    delay(500);  SLEDS_OFF;
+  CAPTIVE_ON;   delay(500);  SLEDS_OFF;
+  BOOT_ON;      delay(500);  SLEDS_OFF;
+  ERROR_ON;     delay(500);  SLEDS_OFF;
+  OK_ON;        delay(500);  SLEDS_OFF;
+
   /* remove saved config and restart if the flash button is pushed */
   attachInterrupt(digitalPinToInterrupt(D3), deleteWifiConfig, CHANGE);
 
-/* Setup PWM */
-  analogWriteFreq(PWM_FREQ);
-  analogWriteRange(1023);
+  /* Setup PWM */
+  /* 
+   * TODO: PWM setup
+   *   - read last saved values from EEPROM (mode, level, r, g, b)
+   *   - set initial values to the last saved values (use pwm_init(pwm_period, pwm_duty_init_with_the_loaded_values, PWM_CHANNELS, io_info);)
+  */  
+//  pwm_init(pwm_period, pwm_duty_init_with_the_loaded_values, PWM_CHANNELS, io_info);
 
- /* Try to load config */
- if (SPIFFS.begin()) {
-    Serial.println(FPSTR(FSMOUNTED));
+  /* Try to load config */
+  if (SPIFFS.begin()) {
     if (SPIFFS.exists(CONFNAME)) {
       //file exists, reading and loading
-      Serial.println(FPSTR(CONFOPENING));
       File configFile = SPIFFS.open(CONFNAME, "r");
       if (configFile) {
-        Serial.println(FPSTR(CONFOPENED));
         size_t size = configFile.size();
         // Allocate a buffer to store contents of the file.
         std::unique_ptr<char[]> buf(new char[size]);
@@ -171,26 +195,23 @@ void setup() {
         JsonObject& json = jsonBuffer.parseObject(buf.get());
         json.printTo(Serial);
         if (json.success()) {
-          Serial.println(FPSTR(CONFPARSED));
           stassid = String((const char*)json["sta_ssid"]);
           stapass = String((const char*)json["sta_pass"]);
           mqtt_channel = String((const char*)json["mqtt_channel"]);
           mqtt_fname = String((const char*)json["mqtt_fname"]);
           mqtt_user = String((const char*)json["mqtt_user"]);
           mqtt_pass = String((const char*)json["mqtt_pass"]);
-          
+
           hasSavedConfig = true;
         } else {
-          Serial.println(FPSTR(CONFPARSEFAIL));
+          Serial.println(F("Configuration parse failed!"));
         }
       }
     } else {
-      Serial.println(FPSTR(NOCONF));
+      Serial.println(F("No stored config. Starting captive portal..."));
     }
   } else {
-    Serial.println(FPSTR(MOUNTFAIL));
-    Serial.println(FPSTR(RESET));
-    Serial.println("");
+    Serial.println(F("Failed to mount SPIFFS"));
     ESP.reset();
   }
 
@@ -206,7 +227,7 @@ void setup() {
     wfMan.setSaveConfigCallback(saveWifiConfigCallback);
     wfMan.setMinimumSignalQuality(10);
     wfMan.setBreakAfterConfig(true);
-    //wfMan.setDebugOutput(false);        // for final installation
+    wfMan.setDebugOutput(true);        // for final installation
 
     // Custom parameters
     wfMan.addParameter(&mqttchannel);
@@ -214,59 +235,85 @@ void setup() {
     wfMan.addParameter(&mqttuser);
     wfMan.addParameter(&mqttpass);
 
+    WiFi.disconnect(true);
     wfMan.startConfigPortal(ssid.c_str());
   }
 
   /* Connect to the AP if not connected by the WiFiManager */
-  WiFi.setAutoConnect(false);
+  WiFi.setAutoConnect(true);
   WiFi.setAutoReconnect(true);
-  for (uint8_t count = MAX_CONNTRY; count > 0; count--) {
-    Serial.println(FPSTR(WIFICONNECTTRY));
-    if (!WiFi.isConnected()) {
-      WiFi.begin(stassid.c_str(), stapass.c_str());
-    } else {
-      Serial.println(FPSTR(WIFIFAIL));
-      break;
-    }
-    delay(500);
-  }
-  
+
+  delay(500);
+  Serial.println(F("Connecting to the AP..."));
+  WiFi.begin(stassid.c_str(), stapass.c_str());
+  delay(500);
   // Check for wifi connected status after a number of tries
   if (!WiFi.isConnected()) {
     SLEDS_OFF;
-    Serial.print(FPSTR(CANNOT_CONNECT));
-    Serial.println(stassid.c_str());
-    Serial.println(FPSTR(CONFSTR));
     WiFi.printDiag(Serial);
-    Serial.println(FPSTR(RESETNODE));
     ERROR_ON;
+    delay(1000);
+    Serial.println(F("Cannot connect to the AP. Resetting..."));
+    ESP.reset();
   } else {
     SLEDS_OFF;
     WiFi.setAutoConnect(true);
-    Serial.println("");
-    Serial.println(FPSTR(WIFIOK));
-    Serial.println(FPSTR(WIFICONF));
+    WiFi.setAutoReconnect(true);
     WiFi.printDiag(Serial);
-    Serial.print(FPSTR(WIFIIP));
-    Serial.println(WiFi.localIP());
     OK_ON;
   }
 
-/*
-    TODO: Setup the MQTT client and process incoming messages
-*/
+  /* Initializing the connection with the MQTT broker */
+  mqttC.setServer(WiFi.gatewayIP(), MQTT_SERVER_PORT);
+  mqttC.setCallback(mqttCallback);
+  connectMQTT(mqtt_user.c_str(), mqtt_pass.c_str(), mqtt_channel.c_str(), mqtt_fname.c_str());
+  if (!mqttC.connected()) {
+    SLEDS_OFF;
+    Serial.println(F("Initial connection to the MQTT broker has been failed."));
+    ERROR_ON;
+  } else {
+    if (WiFi.isConnected()) {
+      SLEDS_OFF;
+      Serial.println(F("WiFi and MQTT has been conneted successfully. Processing..."));
+      OK_ON;
+    }
+  }
+}
+
+bool connectMQTT(const char* user, const char* pass, const char* topic, const char* clientid) {
+  bool res = false;
+  int connTry = MAX_CONNTRY;
+  while (connTry--) {
+    mqttC.connect(clientid, user, pass);
+    delay(50);
+    if (mqttC.connected()) {
+      mqttC.subscribe(topic);
+      Serial.println(F("Connected to the MQTT broker."));
+      res = true;
+      break;
+    }
+  }
+  return res;
+}
+
+void mqttCallback(char* topic, uint8_t* payload, unsigned int len) {
+  payload[len] = 0;
+  Serial.print(F("Payload received: "));
+  Serial.println((const char*)payload);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  SLEDS_OFF;
   if (WiFi.isConnected()) {
-    SLEDS_OFF;
-    delay(5);
-    OK_ON;
+    if (mqttC.connected()) {
+      mqttC.loop();
+      BOOT_ON;
+    } else {
+      Serial.println(F("Reconnecting to the MQTT broker..."));
+      connectMQTT(mqtt_user.c_str(), mqtt_pass.c_str(), mqtt_channel.c_str(), mqtt_fname.c_str());
+    }
   } else {
-    SLEDS_OFF;
-    delay(5);
     ERROR_ON;
   }
-  delay(200);
+  delay(50);
 }
