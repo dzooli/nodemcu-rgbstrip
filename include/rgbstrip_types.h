639d18a5de060f6e88d656f5509a06973a9488d4
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2022-12-26 14:49:03 +0100

    project specific types

---
+++ include/rgbstrip_types.h
@@ -0,0 +1,25 @@
+#ifndef __RGBSTRIP_TYPES_H__
+
+#define __RGBSTRIP_TYPES_H__
+
+struct rgbstatus
+{
+    unsigned char r, g, b, l;
+};
+
+/* Program states and the current state */
+/*
+ * Not used yet
+ *
+enum {
+  MODE_OFF,
+  MODE_ON,
+  MODE_FULL,
+  MODE_SLEEP,
+  MODE_FLASH,
+  MODE_SMOOTH,
+  MODE_LOVE
+};
+*/
+
+#endif
