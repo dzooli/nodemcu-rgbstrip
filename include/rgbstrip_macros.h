5a7040d720b0d61c09963abaa165e454ac27caf2
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2022-12-27 02:16:25 +0100

    selfTest() moved to here

--- include/rgbstrip_macros.h
+++ include/rgbstrip_macros.h
@@ -2,35 +2,58 @@
 
 #define __RGBSTRIP_MACROS_H__
 
+#include <Arduino.h>
+
 #define SLEDS_OFF                \
     {                            \
-        digitalWrite(D6, false); \
-        digitalWrite(D7, false); \
-        digitalWrite(D8, false); \
+        digitalWrite(D2, false); \
+        digitalWrite(D5, false); \
+        digitalWrite(D4, false); \
     }
 #define CAPTIVE_ON               \
     {                            \
-        digitalWrite(D6, false); \
-        digitalWrite(D7, false); \
-        digitalWrite(D8, true);  \
+        digitalWrite(D2, false); \
+        digitalWrite(D5, false); \
+        digitalWrite(D4, true);  \
     } // Blue LED
 #define BOOT_ON                  \
     {                            \
-        digitalWrite(D6, true);  \
-        digitalWrite(D7, true);  \
-        digitalWrite(D8, false); \
+        digitalWrite(D2, true);  \
+        digitalWrite(D5, true);  \
+        digitalWrite(D4, false); \
     } // Yellow LED
 #define ERROR_ON                 \
     {                            \
-        digitalWrite(D6, true);  \
-        digitalWrite(D7, false); \
-        digitalWrite(D8, false); \
+        digitalWrite(D2, true);  \
+        digitalWrite(D5, false); \
+        digitalWrite(D4, false); \
     } // Red LED
 #define OK_ON                    \
     {                            \
-        digitalWrite(D6, false); \
-        digitalWrite(D7, true);  \
-        digitalWrite(D8, false); \
+        digitalWrite(D2, false); \
+        digitalWrite(D5, true);  \
+        digitalWrite(D4, false); \
     } // Green LED
+
+inline void selfTest()
+{
+    // STATUS LED TEST
+    SLEDS_OFF
+    delay(100);
+    SLEDS_OFF
+    CAPTIVE_ON
+    delay(100);
+    SLEDS_OFF
+    BOOT_ON
+    delay(100);
+    SLEDS_OFF
+    ERROR_ON
+    delay(100);
+    SLEDS_OFF
+    OK_ON
+    delay(100);
+    SLEDS_OFF
+}
+
 
 #endif
