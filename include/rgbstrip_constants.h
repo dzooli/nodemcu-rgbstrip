40834dd774a4d30cb36b52afe6abdf9c708f9936
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2022-12-27 14:07:00 +0100

    constants separation

---
+++ include/rgbstrip_constants.h
@@ -0,0 +1,20 @@
+#ifndef __RGBSTRIP_CONST_H__
+
+#define __RGBSTRIP_CONST_H__
+
+/* Global constants */
+#define MQTT_SERVER_PORT 1883
+#define MAX_CONNTRY 5
+#define PWM_FREQ 100
+#define CONFNAME "/wificreds.conf"
+#define STATFILE "/status.conf"
+
+/*
+   Definitions for the MQTT parameters
+*/
+#define MQTT_CLEN 80 // channel string length
+#define MQTT_ULEN 20 // username length
+#define MQTT_PLEN 20 // password length
+#define MQTT_FLEN 20 // friendly name length
+
+#endif
